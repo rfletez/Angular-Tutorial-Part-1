24ad480ebe8067408b10174998d57364ffa911d9
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-01-04 08:24:09 -0700

    Added books to the Component

--- AngularDemo/MySecondDemo/eCommerceDemo/src/app/book/book.component.ts
+++ AngularDemo/MySecondDemo/eCommerceDemo/src/app/book/book.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book.component.css']
 })
 export class BookComponent implements OnInit {
+  name: string = "Clean Code";
+  author: string = "Rober C. Martin";
+  src: string = "https://m.media-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg";
+
+  name2: string = "System Design Interview - An Insider's Guide";
+  author2: string = "Alex Wu";
+  src2: string = "https://m.media-amazon.com/images/I/312L17hmrOL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg";
 
   constructor() { }
 
