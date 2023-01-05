eb737cf7061478f97726247cd6ec39a69c87e13c
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-01-05 09:10:20 -0700

    Practiced on ngFor statement

--- AngularDemo/MySecondDemo/eCommerceDemo/src/app/book/book.component.ts
+++ AngularDemo/MySecondDemo/eCommerceDemo/src/app/book/book.component.ts
@@ -1,22 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Book {
+  name: string;
+  author: string;
+  src: string;
+}
+
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.css']
 })
 export class BookComponent implements OnInit {
-  name: string = "Clean Code";
-  author: string = "Rober C. Martin";
-  src: string = "https://m.media-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg";
-
-  name2: string = "System Design Interview - An Insider's Guide";
-  author2: string = "Alex Wu";
-  src2: string = "https://m.media-amazon.com/images/I/312L17hmrOL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg";
+  books: Book[] = [
+    {
+      name: "Clean Code",
+      author: "Rober C. Martin",
+      src: "https://m.media-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg"
+    },
+    {
+      name: "System Design Interview - An Insider's Guide",
+      author: "Alex Wu",
+      src: "https://m.media-amazon.com/images/I/312L17hmrOL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg"
+    }
+  ];
 
   isSubmitted: boolean = false;
 
   isDisplayed: boolean = true;
+
 
   constructor() { }
 
