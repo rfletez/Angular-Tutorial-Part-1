226b2f25cb19e7b3a9a71fc12f382d77e20d7e97
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-01-09 12:52:16 -0700

    Lifecylce Hooks
    Following along freeCodeCamp Angular tutorial.

--- AngularDemo/MySecondDemo/HelloWorld/src/app/app.component.ts
+++ AngularDemo/MySecondDemo/HelloWorld/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'HelloWorld';
+
+  intervalSub: any = null;
+
+  ngOnInit(): void {
+    this.intervalSub = setInterval(() => {
+      console.log("Starting Point.");
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    if(this.intervalSub) {
+      clearInterval(this.intervalSub);
+    }
+  }
+
+  //Lifecycle hooks can help with the behavior of the components during the 
+  //creation, updation, and destruction phase.
 }
