8a11cb1edfd24556fb537cb9cc47f655647070f2
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-02-01 08:56:26 -0700

    Completed Grouping of Form Controls by Procademy

--- freeCodeCamp/fullCourse/src/app/reactive/reactive.component.ts
+++ freeCodeCamp/fullCourse/src/app/reactive/reactive.component.ts
@@ -9,11 +9,16 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class ReactiveComponent implements OnInit {
   reactiveForm!: FormGroup;
 
+  constructor(private formGroup: FormGroup) {}
+
   ngOnInit() {
     this.reactiveForm = new FormGroup({
-      fname: new FormControl(null, Validators.required),
-      lname: new FormControl(null, Validators.required),
-      email: new FormControl(null, [Validators.required, Validators.email]),
+      personalDetails: new FormGroup({
+        fname: new FormControl(null, Validators.required),
+        lname: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email])
+      }),
+
       gender: new FormControl("male"),
       country: new FormControl("usa"),
       hobbies: new FormControl("movies")
