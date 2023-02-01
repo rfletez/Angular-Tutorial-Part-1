03db81a23b63a5aa92a1d4f9a4540cc9e0064ccd
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-02-01 08:16:35 -0700

    Completed Form Validation of Reactive Forms by Procademy
    Number 90 in his long list of videos in order.

--- freeCodeCamp/fullCourse/src/app/app.module.ts
+++ freeCodeCamp/fullCourse/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +22,8 @@ import { ReactiveComponent } from './reactive/reactive.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [RecordsService],
   bootstrap: [AppComponent]
--- freeCodeCamp/fullCourse/src/app/reactive/reactive.component.ts
+++ freeCodeCamp/fullCourse/src/app/reactive/reactive.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
   styleUrls: ['./reactive.component.css']
 })
-export class ReactiveComponent {
+export class ReactiveComponent implements OnInit {
+  reactiveForm!: FormGroup;
 
+  ngOnInit() {
+    this.reactiveForm = new FormGroup({
+      fname: new FormControl(null, Validators.required),
+      lname: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      gender: new FormControl("male"),
+      country: new FormControl("usa"),
+      hobbies: new FormControl("movies")
+    });
+  }
+
+  onSubmit() {
+    console.log(this.reactiveForm);
+  }
 }
